568ff55d6760762107ed60e433ce26fe88a96799
Luiz Lopes <luizlopes12@outlook.com>
2022-02-23 18:11:31 -0300

    Update index.jsx

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,12 +1,16 @@
 import React,{useState, useEffect} from 'react'
 import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
 const Main = () => {
   return (
     <>
     <h1>To do list</h1>
-    <form action="#">
-        <TextField id="outlined-basic" label="Tarefa" variant="outlined" />
+    <form onSubmit={(e)=>e.preventDefault()}>
+        <TextField id="outlined-basic" label="Tarefa" variant="outlined"/>
+        <Button  variant='outlined'>Adicionar</Button>
     </form>
+
     </>
   )
 }
