43305bc621daed7a57a010b9575fe7610aeeee90
Luiz Lopes <luizlopes12@outlook.com>
2022-02-23 23:54:41 -0300

    Update index.jsx

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -2,13 +2,33 @@ import React,{useState, useEffect} from 'react'
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 
-const Main = () => {
+const Main = (props) => {
+  const [input, setInput] = useState('')
+  const [todos, setTodos] = useState([])
+  const handleChange = (e) =>{
+    setInput(e.target.value)
+  }
+  const handleSubmit = (e)=>{
+    e.preventDefault()
+    props.onSubmit({
+      id: Math.floor(Math.random() * 1000),
+      text: input
+    }
+    )
+    setInput('')
+  }
   return (
     <>
-    <h1>To do list</h1>
-    <form onSubmit={(e)=>e.preventDefault()}>
-        <TextField id="outlined-basic" label="Tarefa" variant="outlined"/>
-        <Button  variant='outlined'>Adicionar</Button>
+    <h1>What's the plan for today?</h1>
+    <form onSubmit={handleSubmit}>
+        <TextField 
+          id="outlined-basic" 
+          label="Tarefa" 
+          variant="outlined"
+          value={input}
+          onChange={handleChange}
+          />
+        <Button variant='outlined'>Adicionar</Button>
     </form>
 
     </>
