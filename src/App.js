e8316198e85602a4cb771cac5b9b3b42b73357df
Luiz Lopes <luizlopes12@outlook.com>
2022-02-24 12:05:46 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,41 @@
-import {GlobalStyle} from './global/GlobalStyle';
+import { GlobalStyle } from "./global/GlobalStyle";
+import { useState } from "react";
 function App() {
+  const [lista, setLista] = useState([]);
+  const [input, setInput] = useState("");
+
+  const handleChange = (e) => {
+    setInput(e.target.value);
+    console.log(input);
+  };
+  const handleAdd = () => {
+    setLista([...lista, input]);
+    setInput("");
+  };
+  const formSubmit = (e) => {
+    e.preventDefault();
+  };
+  const excluirItem = (index) => {
+    let tmpArray = [...lista];
+    tmpArray.splice(index, 1);
+    setLista(tmpArray);
+  };
   return (
     <>
-      <GlobalStyle/>
+      <GlobalStyle />
+      <form onSubmit={formSubmit}>
+        <input onChange={handleChange} value={input} />
+        <button onClick={handleAdd}>Adicionar</button>
+      </form>
+      <ul>
+        {lista &&
+          lista.map((item, index) => (
+            <li key={index}>
+              {item}
+              <button onClick={()=>excluirItem(index)}>Excluir</button>
+            </li>
+          ))}
+      </ul>
     </>
   );
 }
