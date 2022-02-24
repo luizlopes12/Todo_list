b313b88a6eb40a549951960c079b8e59d94e244f
Luiz Lopes <luizlopes12@outlook.com>
2022-02-24 15:32:53 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import { GlobalStyle } from "./global/GlobalStyle";
-import { useEffect, useState } from "react";
-import {
-        Button, 
-        TextField,
-        List,
-        ListItem,
-        ListItemText,
-      } from '@mui/material'
-import {Container, ListContainer} from './style'
+import { useState } from "react";
+import { Button, TextField, List, ListItem, ListItemText } from "@mui/material";
 
-const App = () =>{
+import { Container, ListContainer } from "./style";
+
+const App = () => {
   const [lista, setLista] = useState([]);
   const [input, setInput] = useState("");
-
   const handleChange = (e) => {
     setInput(e.target.value);
   };
@@ -20,47 +14,59 @@ const App = () =>{
     setLista([...lista, input]);
     setInput("");
   };
-  const formSubmit = (e) => {
-    e.preventDefault();
-  };
   const excluirItem = (index) => {
     let tmpArray = [...lista];
     tmpArray.splice(index, 1);
     setLista(tmpArray);
   };
+  const formSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <Container>
       <GlobalStyle />
       <h1>O que vamos fazer hoje?</h1>
       <form onSubmit={formSubmit}>
-        <TextField 
-          style={{width: '100%'}}
-          color='secondary'
+        <TextField
+          style={{ width: "100%" }}
+          color="secondary"
           label="Tarefa"
-          size='small'
-          onChange={handleChange} 
-          value={input} 
-          />
+          size="small"
+          onChange={handleChange}
+          value={input}
+        />
         <Button
-         variant="contained"
+          variant="contained"
           color="success"
           onClick={input.length > 0 ? handleAdd : undefined}
-          >Adicionar</Button>
+        >
+          Adicionar
+        </Button>
       </form>
       <ListContainer>
         {lista &&
           lista.map((item, index) => (
-          <List key={index}>
-            <ListItem disablePadding>
-              <ListItemText primary={item} style={{width: '100%', wordWrap: 'break-word'}}/>
-                <Button onClick={()=>excluirItem(index)}  color='error' variant='contained' size='small'>Excluir</Button>
-            </ListItem>
-          </List>
+            <List key={index}>
+              <ListItem disablePadding>
+                <ListItemText
+                  primary={item}
+                  style={{ width: "100%", wordWrap: "break-word" }}
+                />
+                <Button
+                  onClick={() => excluirItem(index)}
+                  color="error"
+                  variant="contained"
+                  size="small"
+                >
+                  Excluir
+                </Button>
+              </ListItem>
+            </List>
           ))}
       </ListContainer>
-
     </Container>
   );
-}
+};
 
 export default App;
